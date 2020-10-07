e3a6e026ac3452313694fa8f0d80ac485ba43dca
Rodriguez Manuel Joaquin <manurodriguez@itba.edu.ar>
2020-10-07 16:25:21 -0300

    hola mundo

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <TopBar msg="Hola mundo"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TopBar from '@/components/TopBar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    TopBar
+  }
+}
+</script>
