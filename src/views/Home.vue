c2f3d307c32c2136eb6eb8528df8c255bb15b536
vriera <57446063+vriera@users.noreply.github.com>
2020-10-08 18:06:06 -0300

    Arreglos, registros
    Cambie como funcionan las columnas en distintos tamaños. El registro está mas disponible

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,37 +3,52 @@
     <NavBar/>
 
     <div class="home">
+      <v-responsive>
       <v-container>
         <v-layout row justify-space-around>
-          <v-col lg="6" class="d-flex">
-            <v-card class="elevation-10" v-responsive color=#F3BB96>
-              <v-img :src="require('../assets/images/home1.png')" alt="Persona haciendo ejercicio" class="hidden-sm-and-down" >
+          <v-col md="6" xs="12" sm="10" class="d-flex">
+            <v-card class="elevation-10 hidden-sm-and-down" color=#F3BB96>
+              <v-img :src="require('../assets/images/home1.png')" alt="Persona haciendo ejercicio">
                 <p>Armá rutinas fácil y compartilas con tus alumnos</p>
               </v-img>
             </v-card>
           </v-col>
-          <v-col lg="6" class="d-flex" >
-            <v-card class="elevation-10" v-responsive>
-              <div class="rowb">
-                <div class="columnb">
-                  <h1 class="align-center">
-                    Registrate!
-                  </h1>
-                  <div class="input">
-                    <input  type="text" value="Nombre">
-                  </div>
-                  <div class="input">
-                    <input type="password" value="Contraseña">
+          <v-col md="6" xs="12" sm="10" class="d-flex" >
+            <v-card class="elevation-10">
+              <div class="rowCustom">
+                <div class="columnCustom">
+                  <div class="reg">
+                    <h1 class="align-center">
+                      Registrate!
+                    </h1>
+                    <div class="input">
+                      <input  type="text" value="Nombre">
+                    </div>
+                    <div class="input">
+                      <input type="password" value="Contraseña">
+                    </div>
+                    <div>
+                      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" id="vistazoLink"> Echarle un vistazo</a>
+                    </div>
+                    <div >
+                      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                        <div class="regButton">
+                          Registrarse
+                        </div>
+                      </a>
+                    </div>
                   </div>
                 </div>
-                <div class="columnb">
+                <div class="columnCustom">
                   <v-img id="imgLogo" :src="require('../assets/images/logo.png')" alt="Logo" contain class="pt-auto pb-auto pr-auto"/>
                 </div>
               </div>
             </v-card>
+
           </v-col>
         </v-layout>
       </v-container>
+      </v-responsive>
     </div>
   </v-app>
 </template>
@@ -65,31 +80,58 @@ p {
 
 #imgLogo{
   vertical-align: middle;
-  right: -20px;
+  right: -15px;
   height: 300px;
   width: 300px;
 }
-
-.rowb {
+#vistazoLink{
+  color:gray;
+  position: relative;
+  left: 15px;
+  bottom: 1px;
+  margin: 15px;
+  font-family: NotoSansRegular;
+}
+.regButton {
+  font-family: NotoSansRegular;
+  background-color: gray;
+  border: none;
+  color: white;
+  padding: 5px 50px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  position: relative;
+  left: 10px;
+}
+.rowCustom {
   display: flex;
   margin: 10%;
 }
 
-.columnb {
+.columnCustom {
   float: left;
   width: 50%;
 }
 
 /* Clear floats after the columns */
-.rowb:after {
+.rowCustom:after {
   content: "";
   display: table;
   clear: both;
 }
+
 .input{
   margin: 7px;
   border: 2px solid black;
   border-radius: 7px;
   font-size: 1em;
+}
+.reg{
+  vertical-align: middle;
+  padding-bottom: 0;
+  padding-top: 0;
+  position:center;
 }
 </style>
