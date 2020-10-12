a07218b46bf3e2670c9ae3d49d45ffd5e53bc8bf
szavalia <szavalia@itba.edu.ar>
2020-10-12 17:48:29 -0300

    Todo listo, capitán
    En Clients, por si Ana no entiende

--- src/components/ClientEntry.vue
+++ src/components/ClientEntry.vue
@@ -1,9 +1,9 @@
 <!-- Mostrar info del cliente:  Imágen, Nombre, Edad, Rutina actual, Progreso y una opción para chatear-->
 
 <template>
-  <div >
-    <v-row style="width: 700px;">
-      <v-col cols="1" :align-self="align">
+  <div style="min-width: 100%">
+    <v-row style="padding-left: 50px;">
+      <v-col cols="1" >
         <v-avatar contain max-height="50px" >
           <img :src="cliente.imagen" alt="imagen">
         </v-avatar>
@@ -14,17 +14,22 @@
       <v-col cols="1">
           <p>{{cliente.edad}}</p>
       </v-col>
-      <v-col cols="3">
+      <v-col cols="2">
           <p>{{cliente.rutinaActual}}</p>
       </v-col>
+      <v-col cols="3" style="margin-top: 25px">
+        <v-progress-linear :value="cliente.progreso" background-color="grey lighten-2" color="yellow darken-2"></v-progress-linear>
+      </v-col>
       <v-col cols="1">
-          <p>{{cliente.progreso + '%'}}</p>
+        <v-btn icon to="/messages" style="margin-top: 8px"> <v-icon>{{'mdi-message-outline'}}</v-icon></v-btn>
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
+import '@mdi/font/css/materialdesignicons.css'
+
 export default {
 name: "ClientEntry",
 props: ['cliente'],
@@ -32,12 +37,20 @@ props: ['cliente'],
     return{
 
     }
-  }
+  },
+  icons: {
+    iconfont: 'md',
+  },
 }
 </script>
 
 <style scoped>
 p{
   margin-top: 10px;
+  font-family: NotoSansRegular;
+  color: #626262;
+  font-size: 20px;
+  text-transform: none;
+  justify-content: left;
 }
 </style>
