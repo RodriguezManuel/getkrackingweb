bd37ff2d0914f89fcc75ce9f7296c95c38720148
szavalia <szavalia@itba.edu.ar>
2020-10-22 23:02:26 -0300

    Anda mandar muchos mails

--- src/api/user.js
+++ src/api/user.js
@@ -25,19 +25,26 @@ class UserApi {
     static async login(credentials, controller) {
         const result = await Api.post(`${UserApi.url}/login`, false, credentials, controller);
         sessionStorage.setItem('token', result.token);
-        Api.token = result.token;
         return result;
     }
     static async verify(v, controller){
         const result = await Api.post(`${UserApi.url}/verify_email`, false, v, controller);
         sessionStorage.setItem('token', result.token);
-        Api.token = result.token;
+    }
+
+    static async getCurrent(controller){
+    return await Api.get(`${UserApi.url}/current`, true, controller);
     }
 
     static async logout(controller) {
         await Api.post(`${UserApi.url}/logout`, true, controller);
         Api.token = undefined;
         sessionStorage.setItem('token', Api.token);
+    }
+
+    static async resend(email, controller){
+        let aux = {email: email};
+        await Api.post(`${UserApi.url}/resend_verification`, false, aux, controller);
     }
 }
 
--- src/views/notLogged/VerifCorrecta.vue
+++ src/views/notLogged/VerifCorrecta.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <h1> Insertar el codigo de verificación</h1>
-    <input v-model="email" placeholder="tu mail"/>
-    <input v-model="code" placeholder="tu código"/>
-    <p>{{user}}, {{token}}</p>
-    <p>{{email}}, {{code}}</p>
+  <div style="vertical-align:middle; horiz-align: center; ">
+        <div>
+          <titulo>¡YA CASI ESTÁ LISTO!</titulo>
+        </div>
+        <texto> Insertá tu mail:</texto>
+        <v-text-field
+            label="Email"
+            placeholder="Email"
+            size="27%" outlined
+            :rules="[rules.required(email), rules.email(email)]"
+            hide-details="auto"
+            v-model="email"
+        ></v-text-field>
+        <texto> Insertá el codigo de verificación:</texto>
+        <v-text-field
+            label="Code"
+            placeholder="Code"
+            size="27%" outlined
+            :rules="[rules.required(code), rules.counterCode(code)]"
+            hide-details="auto"
+            v-model="code"
+        ></v-text-field>
     <v-btn v-on:click="verif()" style="color:#FBAC31"> verificar</v-btn>
   </div>
 </template>
@@ -21,16 +37,18 @@ export default {
       token: this.$route.query.token,
       code: '',
       email:'',
+      rules: {
+        required: value => !!value || 'Requerido.',
+        counterCode: value => value.length ==6,
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Formato de mail invalido.'
+        },
+      }
     }
   },
   methods: {
     async verif(){
-      /*
-      Api.setToken(this.token);
-      console.log("ya setie token");
-      console.log(Api.token);
-      const email = UserApi.getCurrent().email;
-      */
       // eslint-disable-next-line no-undef
       const v = new ToVerify(this.email, this.code);
       UserApi.verify(v, null);
@@ -41,5 +59,23 @@ export default {
 </script>
 
 <style scoped>
+
+titulo {
+  font-family: NotoSans-Bold;
+  font-size: 35px;
+  color: #FBAC31;
+  text-align: center;
+  margin-top: 15px;
+}
+texto {
+  font-family: NotoSans-Regular;
+  font-size: 20px;
+  color: #2B2B2B;
+  text-align: left;
+  margin-left: 15px;
+  margin-right: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 
 </style>
